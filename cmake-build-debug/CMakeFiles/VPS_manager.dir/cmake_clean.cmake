111f7f000b129886a78feceae52cc4ba8482c698
Aadniz <kristianaa25@protonmail.com>
2022-06-19 14:53:56 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/VPS_manager.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VPS_manager.dir/main.cpp.o"
+  "VPS_manager"
+  "VPS_manager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VPS_manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
